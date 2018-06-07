803bd68fdd2a948367ffe114627237bd2ecdbffd
chencl0214 <chenchuanlu0214@hotmail.com>
2018-06-07 21:29:42 +0800

    123

--- ChangeClient.py
+++ ChangeClient.py
@@ -69,3 +69,4 @@ if __name__ == '__main__':
     testunit = unittest.TestLoader().loadTestsFromTestCase(ChangeClient)
     unittest.TextTestRunner(verbosity=2).run(testunit)
     #unittest.main()
+#########
